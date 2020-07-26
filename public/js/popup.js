03cda52fc15bc53c49eb2d80272aee40174c1e67
gleb <gleb573802wow@mail.ru>
2020-07-26 22:53:47 +0300

    draggable message popup done

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,25 +1,53 @@
-// const modalTriggers = document.querySelectorAll('.popup-trigger')
-// const modalCloseTrigger = document.querySelector('.popup-modal__close')
-// const bodyBlackout = document.querySelector('.body-blackout')
+// Message div
+// Make the DIV element draggable:
+dragElement(document.getElementById("message-div"));
 
-// modalTriggers.forEach(trigger => {
-//   trigger.addEventListener('click', () => {
-//     const { popupTrigger } = trigger.dataset
-//     const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`)
+function dragElement(element) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(element.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(element.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV: 
+    element.onmousedown = dragMouseDown;
+  }
 
-//     popupModal.classList.add('is--visible')
-//     bodyBlackout.classList.add('is-blacked-out')
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
 
-//     popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
-//        popupModal.classList.remove('is--visible')
-//        bodyBlackout.classList.remove('is-blacked-out')
-//     })
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    element.style.top = (element.offsetTop - pos2) + "px";
+    element.style.left = (element.offsetLeft - pos1) + "px";
+  }
 
-//     bodyBlackout.addEventListener('click', () => {
-//       // TODO: Turn into a function to close modal
-//       popupModal.classList.remove('is--visible')
-//       bodyBlackout.classList.remove('is-blacked-out')
-//     })
-//   })
-// })
-alert(1);
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+//fetch
+const button = document.querySelector('form.message>input[type=submit]');
+console.log(button);
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('im here');
+})
