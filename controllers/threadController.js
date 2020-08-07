9667ec7a8bce5de20e3401abd6b613f725d26459
gleb <gleb573802wow@mail.ru>
2020-08-07 13:59:16 +0300

    thread controller rewritten with async/await

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -4,50 +4,53 @@ const Board = require('../models/board');
 
 // get a form for thread creation with boardId as a parameter
 const create_thread_get = (req, res) => {
-  board = req.query.board;
-  res.render('create-thread', { title: "Create a thread", board })
+  try {
+    board = req.query.board;
+    res.render('create-thread', { title: "Create a thread", board })
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 
 // create new thread
-const create_thread_post = (req, res) => {
-  const thread = new Thread(req.body);
-  thread.save()
-    .then(result => {
-      Board.findByIdAndUpdate(req.body.board, { $addToSet: { threads: result._id } })
-        .then(
-          result => { return result }
-        )
-        .catch(err => console.log(err));
-      messageController.first_message_in_thread(result._id, req.body.text);
-      res.redirect(`/thread/${result._id}`);
-    })
-    .catch(err => console.log(err));
+const create_thread_post = async (req, res) => {
+  try {
+    const thread = new Thread(req.body);
+    const savedThread = await thread.save();
+    Board.findByIdAndUpdate(req.body.board, { $addToSet: { threads: savedThread._Id } });
+    messageController.first_message_in_thread(savedThread._id, req.body.text);
+    return res.redirect(`/thread/${savedThread._id}`);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 //get a single thread by id
-const get_thread_by_id = (req, res) => {
-
-  Thread.findById(req.params.id).populate('messages')
-    .then(result => {
-      res.render('thread', { title: "imageboard", thread: result });
-    })
-    .catch(err => console.log(err));
+const get_thread_by_id = async (req, res) => {
+  try {
+    const thread = await Thread.findById(req.params.id).populate('messages');
+    return res.render('thread', { title: 'Imageboard', thread });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 //get all threads by board id
-const get_threads_by_board_id = (id) => {
-  return Thread.find({ boardId: id }).exec();
+const get_threads_by_board_id = async (id) => {
+  try {
+    return await Thread.find({ boardId: id });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
-const find_by_id_and_update = (threadId, messageId) => {
-  return Thread.findByIdAndUpdate(threadId, { $addToSet: { messages: messageId } })
-    .then(result => { return result })
-    .catch(err => console.log(err));
-}
-
-const new_func = () => {
-  return 4;
+const find_by_id_and_update = async (threadId, messageId) => {
+  try {
+    return await Thread.findByIdAndUpdate(threadId, { $addToSet: { messages: messageId } });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 module.exports = {
@@ -55,6 +58,5 @@ module.exports = {
   create_thread_post,
   get_threads_by_board_id,
   get_thread_by_id,
-  find_by_id_and_update,
-  new_func
+  find_by_id_and_update
 }
